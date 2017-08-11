e10a615ae71f9e5c4e156b7264d2870f8c335301
Isashi <isashimitsui@gmail.com>
2017-08-11 21:54:17 +0200

    Finish HTML form part

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		# app/controllers/users_controller.rb
+    def create
+      #@user = User.new(name: params[:name], email: params[:email], password: params[:password])
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to new_user_path
+      else
+        render :new
+      end
+    end
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password)
+	end
+
+end
